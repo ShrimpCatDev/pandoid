80d8ec9ed215ce6b44c9852a1de5615963237cae
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-10-20 21:24:07 -0500

    add build script

---
+++ build.lua
@@ -0,0 +1,9 @@
+return {
+      name = 'Freakout',
+      config = 'nonsteam',
+      version = '1.0',
+      love = '11.5',
+      icon = 'icon.png',
+      platforms = {'windows','linux','macos'},
+      ignore = {}
+}
